41a90ca54703f45b6940c945e02b7f2c314d3d94
Brian4755 <brian.mccune1@yahoo.com>
2022-11-10 07:02:00 -0600

    code refactor

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-// document.addEventListener('DOMContentLoaded', e => {
-//   const h1 = document.querySelector("h1")
-  
-//   let h1Animation = h1.innerText = 'wdf'
-//   let textAnimation = "This is working"
-//   for (let i = 0; i < 10; i++) {
-//     h1Animation += textAnimation[i]
-//     // setTimeout(() => {h1Animation += textAnimation[i]}, 1000)
-//   }
-//   console.log(h1Animation)
-//   return h1Animation
-// })
-
-
-function bruh() {
+function textFlow() {
   let textAnimation = 'I am a junior software engineer with the desire to work with others and continuously learn through challenges'
   for (let i = 0; i < textAnimation.length; i++) {
     setTimeout(() => {
-      setTimeout(() => { document.getElementById("displayDiv").innerHTML += textAnimation[i]}, 100*i)
+      setTimeout(() => { document.getElementById("displayDiv").innerHTML += textAnimation[i]}, 60*i)
     }, 2000)
   }
- console.log(textAnimation[0])
-  }
+}
 
-bruh()
+textFlow()
